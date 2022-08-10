0ad44028d80de207b8528edeaa502ee867c4ea61
satinder <satindersingh472@gmail.com>
2022-08-09 19:17:54 -0600

    updated

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+<script-example></script-example>
+  </div>
+</template>
+
+<script>
+import ScriptExample from '@/components/scriptExample.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ScriptExample
+   
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/scriptExample.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>first tweet is {{tweets[0]}}</h1>
+        <h2>num of friends are {{num_friends}}</h2>
+        <p>am i logged in? <br> the answer is {{logged_in}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                tweets: [`be honest`,`be positive`,`be humble`],
+                num_friends: 500,
+                logged_in: true
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
